ebd6c4ea77c73af8f6ec63cab4aa8b28f499b2b7
Hassan Masood <iamhassanmasood@gmail.com>
2019-04-10 00:33:32 +0500

    created database successfully

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ const PORT = 4000;
 const Todo = require('./schema');
 const ToDoRoutes = express.Router();
 const connection = mongoose.connection;
+app.use(cors());
+app.use(bodyParser.json());
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+
